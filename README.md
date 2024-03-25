520b74e4ae6e784a2a818912fbfd3171837031e4
jxone <jxonesso@gmail.com>
2024-03-25 22:43:37 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# IZITEST
-test point
+# iZiSwap-core
+
+<div align="center">
+  <a href="https://izumi.finance"> 
+    <img width="900px" height="auto" 
+    src="image/logo.png">
+  </a>
+</div>
+
+
+Core contracts for iZiSwap, a next-generation DEX to maximize capital efficiency by supporting concentrated liquidity and realizing Limit Order in a decentralized way.
+[iZiSwap Periphery](https://github.com/izumiFinance/iZiSwap-periphery)  contracts are suggested entrances to interact with the core contracts.  
+
+## Overview
+
+
+<div align="center">
+  <a href="https://izumi.finance"> 
+    <img width="700px" height="auto" 
+    src="image/overview.png">
+  </a>
+</div>
+
+iZiSwap core includes the core logic implementation for swap, liquidity management, and limit orders. Due to the size limitations of individual contracts on most EVM-compatible blockchains, we have modularized the core logic into separate modules. In the main contract, iZiSwapPool, we utilize the delegateCall() method to invoke these modules.
+
+
+More details can be found in the [iZiSwap whitepaper](https://github.com/izumiFinance/izumi-swap-core/blob/main/whitepaper/iZiSwap:%20Building_Decentralized_Exchange_with_Discretized_Concentrated_Liquidity_and_Limit_Order.pdf) and the [Developer Doc](https://developer.izumi.finance).
+
+
+
+## Licensing
+
+The primary license for iZiSwap Core is the Business Source License 1.1 (BUSL-1.1), see [LICENSE](https://github.com/izumiFinance/iZiSwap-core/blob/main/LICENSE). 
+
+
+## Use source code as npm package
+
+```
+$ npm install iziswap_core
+```
+
+An example to usage this package
+
+```
+import '@izumifinance/iziswap_core/contracts/interfaces/IiZiSwapPool.sol';
+
+contract Foo {
+  IiZiSwapPool pool;
+
+  function bar() {
+      // pool.addLimOrderWithY(...)
+      // pool.addLimOrderWithX(...)
+  }
+}
+```
---
+++ hardhat.config.js
@@ -0,0 +1,162 @@
+require("@nomiclabs/hardhat-waffle");
+require('hardhat-contract-sizer');
+require('@nomiclabs/hardhat-etherscan')
+require("@cronos-labs/hardhat-cronoscan")
+
+const secret = require('./.secret.js');
+const sk = secret.sk;
+const apiKey = secret.apiKey;
+const izumiRpcUrl = "http://47.241.103.6:9545";
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 10
+          },
+          outputSelection: {
+            "*": {
+              "*": [
+                "abi",
+                "evm.bytecode",
+                "evm.deployedBytecode",
+                "evm.methodIdentifiers",
+                "metadata"
+              ],
+            }
+          }
+        }
+      },
+    ]
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+    izumiTest: {
+      url: izumiRpcUrl,
+      accounts: [sk],
+      // gas: 90000000,
+      gasPrice: 100000000000,
+    },
+    bscTest: {
+	    url: 'https://data-seed-prebsc-2-s1.binance.org:8545/',
+      accounts: [sk],
+      // gas: 90000000,
+      gasPrice: 10000000000,
+    },
+    ontologyTest: {
+      url: 'https://polaris1.ont.io:10339',
+      accounts: [sk],
+    },
+    bsc: {
+      url: 'https://bsc-dataseed.binance.org/',
+      accounts: [sk],
+      // gas: 90000000,
+      gasPrice: 5000000000,
+    },
+    ontology: {
+      url: 'https://dappnode1.ont.io:10339',
+      accounts: [sk],
+    },
+    arbitrum: {
+      url: 'https://arb1.arbitrum.io/rpc',
+      accounts: [sk],
+    },
+    cronos: {
+      url: 'https://evm.cronos.org',
+      accounts: [sk],
+    },
+    aurora: {
+      url: 'https://mainnet.aurora.dev',
+      accounts: [sk]
+    },
+    auroraTest: {
+      url: 'https://testnet.aurora.dev',
+      accounts: [sk],
+      gasPrice: 5000000000,
+    },
+    etc: {
+      url: 'https://www.ethercluster.com/etc',
+      accounts: [sk],
+      gasPrice: 1100000000,
+    },
+    polygon: {
+	    url: 'https://rpc-mainnet.maticvigil.com',
+      accounts: [sk],
+    },
+    zkSyncAlphaTest: {
+      url: 'https://zksync2-testnet.zksync.dev',
+      accounts: [sk],
+    },
+    mantleTest: {
+      url: 'https://rpc.testnet.mantle.xyz',
+      accounts: [sk],
+    },
+    mantle: {
+      url: 'https://rpc.mantle.xyz',
+      accounts: [sk],
+    },
+    scrollTestL2: {
+      url: 'https://alpha-rpc.scroll.io/l2',
+      accounts: [sk],
+    },
+    icplazaTest: {
+      url: 'https://rpctest.ic-plaza.org/',
+      accounts: [sk],
+    },
+    icplaza: {
+      url: 'https://rpcmainnet.ic-plaza.org/',
+      accounts: [sk],
+    },
+    syscoin: {
+	    url: 'https://rpc.ankr.com/syscoin',
+      accounts: [sk],
+    },
+    syscoinTest: {
+	    url: 'https://rpc.tanenbaum.io/',
+	    accounts: [sk],
+    },
+    bedrockRolluxTestL2: {
+      url: 'https://bedrock.rollux.com:9545/',
+      accounts: [sk],
+    },
+    meter: {
+      url: 'https://rpc.meter.io',
+      accounts: [sk],
+    },
+    meterTest: {
+      url: 'https://rpctest.meter.io',
+      accounts: [sk],
+    },
+    telos: {
+      url: 'https://mainnet.telos.net/evm',
+      accounts: [sk],
+    },
+    ultron: {
+      url: 'https://ultron-rpc.net',
+      accounts: [sk],
+    },
+    lineaTest: {
+      url: 'https://rpc.goerli.linea.build/',
+      accounts: [sk],
+    },
+    opsideTest: {
+      url: 'https://pre-alpha-us-http-geth.opside.network',
+      accounts: [sk],
+    },
+    opsideTestRollux: {
+      url: 'https://pre-alpha-zkrollup-rpc.opside.network/public',
+      accounts: [sk],
+    },
+  },
+  etherscan: {
+    apiKey: apiKey,
+  }
+};
